30a4f9f4f2d3ba5e937ce372d7158fb138a421e7
mue <mue.miv@gmail.com>
2014-12-22 23:08:57 +0200

    adding feeds panel

---
+++ client/app/scripts/core/components/feed/feeds-panel/feeds-panel.module.js
@@ -0,0 +1,6 @@
+(function(){
+    'use strict';
+    angular.module('rss.core.components.feed.feeds-panel', [
+        'rss.core.web-components'
+    ]);
+})();
--- client/app/scripts/main/app/feed/feed.module.js
+++ client/app/scripts/main/app/feed/feed.module.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('rss.app.feed', [
-
+        'rss.core.components.feed.feeds-panel'
     ]);
 
 })();
